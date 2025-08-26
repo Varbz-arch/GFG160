5a0fd9dfa9386a4efa568178bd37a4efc5e1f0fe
Varbz-arch <archishavarbee@gmail.com>
2025-08-26 23:20:26 +0530

    Add a Classic Stock file

---
+++ Classic_Stock_Buy.py
@@ -0,0 +1,14 @@
+# The cost of stock on each day is given in an array price[].
+# Each day you may decide to either buy or sell the stock i at price[i], 
+# you can even buy and sell the stock on the same day. Find the maximum profit that you can get.
+# Note: A stock can only be sold if it has been bought previously and multiple stocks cannot be held on any given day.
+
+class Solution:
+    def maximumProfit(self, prices) -> int:
+        # code here
+        n = len(prices)
+        profit = 0
+        for i in range(1, n):
+            if prices[i]> prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        return profit
